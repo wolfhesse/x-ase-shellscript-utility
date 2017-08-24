3bf9b5929e0144aa067557df877b6debbb97fe95
Roger Andel <rogera_se@hotmail.com>
2017-08-24 14:09:52 +0000

    Updated s_runner.sh

--- s_runner.sh
+++ s_runner.sh
@@ -1,6 +1,9 @@
 echo running... 
 echo pwd is $PWD
 echo psd-command $(pwd)
-# touch file (running in run.d)
+echo # touch file (running in run.d)
 echo touched >.touched
+echo # listing files
+ls -al
+echo # finalizer
 echo .fin!
