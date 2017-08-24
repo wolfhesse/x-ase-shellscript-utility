f6fba05ff1eb90143691660278b011ef0e8b43b1
wolf hesse <wolfgang.schuessel@gmail.com>
2017-08-24 19:43:13 +0200

    Update s_runner.sh

--- s_runner.sh
+++ s_runner.sh
@@ -3,20 +3,22 @@ DS=$(date +"y%Y.%y%m%d.%H%M%S")
 echo $DS
 TS=$(date +"%s")
 echo $TS
+#
 cp s_runner.sh ./archive.d/saved_runner.$DS.$TS.sh
 git remote add gh git@github.com:wolfhesse/x-ase-shellscript-utility.git
 echo pulling gh
 git pull gh builder
+#
 echo pwd is $PWD
 echo psd-command $(pwd)
-echo touch file running in run.d
-echo touched >> ./run.d/.touched
+echo touched > ./run.d/.touched
 date >> ./run.d/.touched
-echo $DS $TS >> ./run.d/.systems
-uname -a >> ./run.d/.systems
+echo $DS $TS > ./run.d/.system
+uname -a >> ./run.d/.system
 echo running utilities
 echo ___ run x_util_001
 sh x_util_001.sh
+#
 echo try: commit to github
 git add -A .
 git commit -m "builder commit $DS $TS"
