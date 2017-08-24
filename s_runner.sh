35060e3e60f307cc8022d0f84c5b0f18c8e44e76
Roger Andel <rogera_se@hotmail.com>
2017-08-24 14:49:43 +0000

    Updated s_runner.sh

--- s_runner.sh
+++ s_runner.sh
@@ -8,9 +8,16 @@ date >> .touched
 echo listing files
 ls -al
 echo ---
-ls -al ./ssh
+ls -al ../_temp
 echo ---
 ls -al /
 echo ---
+echo try: commit to github
+git remote add gh git@github.com:wolfhesse/x-ase-shellscript-utility.git
+git fetch --all
+git add -A ..
+git commit -m 'builder commit'
+git pull gh
+git push gh
 echo finalizer
 echo .fin
