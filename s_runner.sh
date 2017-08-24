13789bb3cba4eac2d0522a06d49c70c0c12dfb24
Roger Andel <rogera_se@hotmail.com>
2017-08-24 15:14:48 +0000

    Updated s_runner.sh

--- s_runner.sh
+++ s_runner.sh
@@ -3,23 +3,23 @@ date
 echo pwd is $PWD
 echo psd-command $(pwd)
 echo touch file running in run.d
-echo touched >> .touched
-date >> .touched
+echo touched >> ./run.d/.touched
+date >> ./run.d/.touched
 echo listing files
 ls -al
 echo ---
-ls -al ../../_temp
+ls -ax ../.
 echo ---
-ls -al /
+ls -ax /
 echo ---
 echo try: commit to github
 git remote add gh git@github.com:wolfhesse/x-ase-shellscript-utility.git
 echo fetching gh
 git fetch gh 
 #--all
-git add -A ..
+git add -A .
 git commit -m 'builder commit'
-git pull gh master
+git pull gh master origin
 git push gh HEAD:builder
 echo finalizer
 echo .fin
