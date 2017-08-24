0aac7ea08b7293a049a4115c6a9cbbd9615b7f72
Roger Andel <rogera_se@hotmail.com>
2017-08-24 17:27:02 +0000

    Updated s_runner.sh

--- s_runner.sh
+++ s_runner.sh
@@ -3,7 +3,7 @@ DS=$(date +"y%Y.%y%m%d.%H%M%S")
 echo $DS
 TS=$(date +"%s")
 echo $TS
-cp s_runner.sh ./archive.d/saved_runner.$DS.$TS
+cp s_runner.sh ./archive.d/saved_runner.$DS.$TS.sh
 git remote add gh git@github.com:wolfhesse/x-ase-shellscript-utility.git
 echo pulling gh
 git pull gh builder
@@ -15,6 +15,7 @@ date >> ./run.d/.touched
 echo $DS $TS >> ./run.d/.systems
 uname -a >> ./run.d/.systems
 echo running utilities
+echo ___ run r_util_001
 sh r_util_001.sh
 echo try: commit to github
 git add -A .
